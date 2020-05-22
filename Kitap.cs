534f1487b85dd42b3506c3bd9f7652366cf5a3bc
emintaha <58628955+emintaha@users.noreply.github.com>
2020-05-22 22:10:36 +0300

    Add files via upload

---
+++ Kitap.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kitabevi.app
+{
+    public class Kitap : Program
+    {
+
+        public Kitap()
+        {
+
+        }
+
+
+
+        public Kitap(string kitapadi , string kitaptürü , string yazaradi , double basimtarihi)
+        {
+            this.SetKitapadi(kitapadi);
+
+            this.SetKitaptürü(kitaptürü);
+
+            this.SetYazaradi(yazaradi);
+
+            this.SetBasimtarihi(basimtarihi);
+
+        }
+        private string kitapadi;
+       
+        private string kitaptürü;
+        
+        private string yazaradi; 
+        
+        private double basimtarihi;
+
+        public double Basimtarihi { get => basimtarihi; set => basimtarihi = value; }
+
+        public double GetKitapadi()
+        {
+            return Basimtarihi;
+        }
+
+        public void SetKitapadi(string value)
+        {
+            kitapadi = value.ToUpper();
+        }
+
+        public string GetKitaptürü()
+        {
+            return kitaptürü;
+        }
+
+        public void SetKitaptürü(string value)
+        {
+            kitaptürü = value.ToUpper();
+        }
+
+        public string GetYazaradi()
+        {
+            return yazaradi;
+        }
+
+        public void SetYazaradi(string value)
+        {
+            yazaradi = value;
+        }
+
+
+        public void KitapBilgi(int [] value) 
+        {
+            
+        
+        }
+        
+
+        public string KitapGetir() => $" Kitap adı:{this.GetKitapadi()}, Kitap türü: {this.GetKitaptürü()}, Yazar Adı: {this.GetYazaradi()} , Basım tarihi: {this.GetBasimtarihi()}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kitabevi.app
+{
+   public class Program
+    {
+
+        
+
+        static void Main(string[] args)
+
+        {
+            
+            Kitap[] kitapevi = new Kitap[0];
+            string cikis = "";
+            while (cikis != "E")
+            {
+                Console.WriteLine("-------------HOŞGELDİN-------------");
+                Console.WriteLine("Kitap eklemek için 1 e \n listeyi görmek için 2 ye tıklayın");
+                int liste = int.Parse(Console.ReadLine());
+                if (liste==1)
+                {
+                    Console.WriteLine("kaç kitap ekleyeceksiniz");
+                    int kitapsayi = int.Parse(Console.ReadLine());
+                    kitapevi = new Kitap[kitapsayi];
+                    for (int i = 0; i < kitapsayi; i++)
+                    {
+                        Kitap ktp1 = new Kitap();
+                        Console.WriteLine("{0}. kitap içi bilgileri giriniz", i+1);
+                        Console.WriteLine($"kitabın adı:");
+                        ktp1.SetKitapadi(Console.ReadLine());
+                        Console.WriteLine($"kitabın yazarı:");
+                        ktp1.SetYazaradi(Console.ReadLine());
+                        Console.WriteLine($"kitabın türü:");
+                        ktp1.SetKitaptürü(Console.ReadLine());
+                        Console.WriteLine("kitabın basıldığı yıl");
+                        ktp1.SetBasimtarihi(int.Parse(Console.ReadLine()));
+                        
+                    }
+
+
+                }
+                else if (liste==2)
+                {
+                 
+                }
+
+            }
+
+
+
+            
+
+
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
